00a0c41f1a6fc7c79505101a4f5702a059d6a822
Oliver Baylis <oli.baylis@gmail.com>
2018-04-23 18:51:11 +0100

    Move data context property

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -4,12 +4,12 @@ using udemyDatingApp.Models;
 namespace udemyDatingApp.Data
 {
     public class DataContext : DbContext
-    {
-        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
-
+    {        
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
         }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
     }
 }
