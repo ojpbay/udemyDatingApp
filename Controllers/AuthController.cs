a489f1965270c65ae3e74a9a9ef12e2008a96133
Oliver Baylis <oli.baylis@gmail.com>
2018-04-24 19:55:42 +0100

    Add auth controller and generate JWT

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using udemyDatingApp.Data;
+using udemyDatingApp.Dtos;
+using udemyDatingApp.Models;
+
+namespace udemyDatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userToRegister)
+        {
+            userToRegister.Username = userToRegister.Username.ToLower();
+
+            if (!string.IsNullOrWhiteSpace(userToRegister.Username))
+            {
+                if (await _repo.UserExists(userToRegister.Username))
+                {
+                    ModelState.AddModelError("Username", "Username already exists");
+                }
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }            
+
+            var userToCreate = new User
+            {
+                Username = userToRegister.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userToRegister.Password);
+
+            //return CreatedAtRoute()
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLogin)
+        {
+            var userFromRepo = await _repo.Login(userForLogin.Username.ToLower(), userForLogin.Password);
+
+            if (userFromRepo == null)
+            {
+                return Unauthorized();
+            }
+
+            // generate token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("super secret key");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.Username)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { tokenString });
+        }
+    }
+}
---
+++ Dtos/UserForRegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace udemyDatingApp.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        [StringLength(25, MinimumLength = 3, ErrorMessage = "You must specify a username between 3 and 20 characters in length")]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify a password between 4 and 8 characters in length")]
+        public string Password { get; set; }
+    }
+}
